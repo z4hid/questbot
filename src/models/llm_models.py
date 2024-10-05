ca644a11c063fcc44028f5f3beacc84bc6368b25
z4hid <zahidhasan9297@gmail.com>
2024-10-05 06:46:15 +0600

    groq model added

---
+++ src/models/llm_models.py
@@ -0,0 +1,26 @@
+from langchain_groq import ChatGroq
+from src.config import load_config
+
+def get_groq_model(model_name: str):
+    """
+    Returns an instance of the specified Large Language Model (LLM) from the Groq Models Hub.
+    
+    Parameters
+    ----------
+    model_name : str
+        The name of the LLM model to be retrieved.
+    
+    Returns
+    -------
+    ChatGroq
+        An instance of the specified LLM.
+    """
+    config = load_config()
+    return ChatGroq(
+        model=model_name,
+        temperature=0.4,
+        max_tokens=4096,
+        timeout=None,
+        max_retries=2,
+        groq_api_key=config['GROQ_API_KEY']
+    )
